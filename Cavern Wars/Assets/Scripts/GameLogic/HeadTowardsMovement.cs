93bfbeb036addc92e6289b01b1f09c80f24fa5b7
Antti M <antti_rekisteroinnit@hotmail.com>
2018-09-22 13:00:45 +0300

    Head towards movement

--- Cavern Wars/Assets/Scripts/GameLogic/HeadTowardsMovement.cs
+++ Cavern Wars/Assets/Scripts/GameLogic/HeadTowardsMovement.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class HeadTowardsMovement : MonoBehaviour
 {
-	void Update ()
-    {
+    [SerializeField]
+    private float _rotationSpeed = 10f;
 
+    private Vector3 _previousPosition;
+    private Vector3 _directionOfMovement;
+    // Rotation that means the spaceship is heading up.
+    private Quaternion _referenceUp;
+
+    private void Start()
+    {
+        _previousPosition = transform.position;
+        _directionOfMovement = Vector3.up;
+        _referenceUp = transform.rotation;
+    }
+
+    void Update ()
+    {
+        _directionOfMovement = transform.position - _previousPosition;
+        if (!Vector3.zero.Equals(_directionOfMovement))
+        {
+            var lookRot = Quaternion.LookRotation(_directionOfMovement, Vector3.back);
+            var rotation = Quaternion.RotateTowards(transform.rotation, lookRot, _rotationSpeed);
+            transform.rotation = rotation;
+        }
 	}
+
+    private void LateUpdate()
+    {
+        _previousPosition = transform.position;
+    }
 }
